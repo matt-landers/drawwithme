2adf61bc54d2f6d04b5843b85a2e204628853d46
Matt Landers <matt@platypi.io>
2018-04-11 01:04:10 -0500

    initial project - partially working

---
+++ Hubs/DrawingHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace drawwithme.Hubs
+{
+    public class DrawingHub: Hub
+    {
+        public async Task Draw(int x, int y, string id) {
+            await Clients.Group(id).SendAsync(
+                "NewPoint", 
+                new Point() {
+                    x = x,
+                    y =y
+                });
+        }
+
+        public async Task JoinCanvas(string id) {
+            await Groups.AddAsync(Context.ConnectionId, id);
+        }
+
+        public class Point {
+            public int x;
+            public int y;
+            public string clientId;
+        }
+    }
+}
